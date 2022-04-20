dd30887ebd475ee40df7494a06412356f9c14912
edArcturus <eyalliron.dolev@uzh.ch>
2022-04-20 10:39:05 +0200

    Comment out converting newlines to spaces

--- scripts/convert_8859-1_utf-8.py
+++ scripts/convert_8859-1_utf-8.py
@@ -3,6 +3,6 @@ import sys
 filename = sys.argv[1]
 with open(filename, 'r', encoding='iso-8859-1') as infile:
     for line in infile:
-        line = line.replace('\n', ' ')
+        # line = line.replace('\n', ' ')
         sys.stdout.write(line)
 
